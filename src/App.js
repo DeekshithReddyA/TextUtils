af78885f97493cf26a86735fd450f74fd781dde7
DeekshithReddyA <deekshithreddy351@gmail.com>
2024-01-14 16:51:18 +0530

    React Learn

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import Alert from './components/Alert';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import { useState } from 'react';
+
 
 function App() {
+  const [alert,setAlert] =useState(null);
+
+  const showAlert = (message,type) =>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+  }
+
+  const[mode,setMode] = useState('light');
+
+  const toggleMode = ()=>{
+    if(mode === 'light')
+    { 
+    setMode('dark')
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+    showAlert("Dark mode has been enabled","success")
+    // document.title = 'TextUtils - Home (Dark Mode)'
+    // setInterval(() => {
+    //   document.title = "TextUtils is Amazing";
+    // }, 2000);
+    // setInterval(() => {
+    //   document.title = "Install TextUtils Now";
+    // }, 1500);
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+      showAlert("Light mode has been enabled","success")
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar logotitle="TextUtils" aboutText="About" mode ={mode} toggleMode={toggleMode}/>
+    {/* <Navbar aboutText='About'/> */}
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    <TextForm heading="Enter the text to analyze" mode={mode} showAlert={showAlert}/>
+    {/* <About/> */}
     </div>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,57 @@
+import React,{useState} from 'react'
+export default function TextForm(props) {
+  const handleUpClick = ()=>{
+    let newText = text.toUpperCase();
+    // console.log("Uppercase was clicked");
+    setText(newText);
+    props.showAlert("Converted to UpperCase","success");
+  }
+
+  const handleLowClick = ()=>{
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to LowerCase","success");
+  }
+
+  const handleClearClick = ()=>{
+    let newText = ""
+    setText(newText);
+  }
+
+  const handleOnChange =(event) =>{
+    // console.log("On change");
+    setText(event.target.value);
+  }
+
+  const removeExtraSpaces = () =>{
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
+    props.showAlert("Removed Extra Spaces","success");
+  }
+  const [text, setText] =useState('');
+  // text ="new text"; // Wrong way to change the state 
+  // setText("new text"); //Correct way to change the state
+
+
+  return (
+    <>
+    <div className="container my-3">
+        <h3>{props.heading}</h3>
+        <div className="mb-3">
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='light'?'white':'black',color:props.mode==='light'?'black':'white'}} id="exampleFormControlTextarea1"  rows="3"></textarea>
+        </div>
+       <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
+       <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to LowerCase</button>
+       <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+       <button className="btn btn-primary mx-1" onClick={removeExtraSpaces}>Remove Extra Spaces</button>
+    </div>
+    <div className="container my-3">
+      <h3>Your text summary</h3>
+      <p>Words-{text.split(" ").length} , Characters-{text.length} </p>
+      <p>{0.1* text.split(" ").length} seconds to read the above text</p>
+      <h4>Preview</h4>
+      <p>{text.length>0?text:'Enter something in the text box above to preview here'}</p>
+    </div>
+    </>
+  );
+}
